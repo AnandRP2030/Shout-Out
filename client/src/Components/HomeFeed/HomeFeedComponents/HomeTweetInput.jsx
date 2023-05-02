35be794acea9d92d9d747cce6db3d50aef3e7aa3
Anand R P <anand.rp2030@gmail.com>
2023-05-02 23:09:52 +0530

    tweet input box completed

--- client/src/Components/HomeFeed/HomeFeedComponents/HomeTweetInput.jsx
+++ client/src/Components/HomeFeed/HomeFeedComponents/HomeTweetInput.jsx
@@ -7,10 +7,16 @@ import {
   Link,
   Text,
   Icon,
-  HStack
+  HStack,
+  Button,
 } from "@chakra-ui/react";
 import style from "../homeFeed.module.css";
 import { IoEarth } from "react-icons/io5";
+import { BsImage } from "react-icons/bs";
+import { CgPoll } from "react-icons/all";
+import { BsEmojiHeartEyes } from "react-icons/bs";
+import { MdSchedule } from "react-icons/all";
+import { IoLocationOutline } from "react-icons/all";
 
 const HomeTweetInput = () => {
   const imageUrl =
@@ -21,19 +27,19 @@ const HomeTweetInput = () => {
       <Grid
         h="200px"
         templateRows="repeat(8, 1fr)"
-        templateColumns="repeat(15, 1fr)"
+        templateColumns="repeat(22, 1fr)"
         gap={4}
         mt={10}
         className={style.tweetBox}
       >
-        <GridItem rowSpan={8} colSpan={2} bg="tomato">
+        <GridItem rowSpan={8} colSpan={2}>
           <Image
             className={style.userProfilePic}
             src={imageUrl}
             alt="Profile"
           />
         </GridItem>
-        <GridItem colSpan={13} rowSpan={1}>
+        <GridItem colSpan={20} rowSpan={1}>
           <Select w="150px" h="24px" className={style.selectAudience}>
             <option style={{ color: "black" }} value="everyone">
               Everyone
@@ -43,24 +49,31 @@ const HomeTweetInput = () => {
             </option>
           </Select>
         </GridItem>
-        <GridItem colSpan={13} rowSpan={2} className={style.inputGrid}>
+        <GridItem colSpan={20} rowSpan={2} className={style.inputGrid}>
           <Input
             variant="unstyled"
             className={style.tweetInput}
             placeholder="What's happening?"
           />
         </GridItem>
-        <GridItem colSpan={13} rowSpan={2}>
-          <Link href="http://www.google.com" style={{textDecoration:"none"}} >
+        <GridItem colSpan={20} rowSpan={2}>
+          <Link href="http://www.google.com" style={{ textDecoration: "none" }}>
             <HStack className={style.replayAudience}>
               <Icon as={IoEarth} boxSize={5} />
               <Text>Everyone can replay</Text>
             </HStack>
           </Link>
         </GridItem>
-        <GridItem colSpan={13} rowSpan={2} >
-          <HStack>
-                
+        <GridItem colSpan={20} rowSpan={2}>
+          <HStack className={style.tweetLine}>
+            <HStack className={style.tweetIcons}>
+              <Icon as={BsImage} boxSize={5} />
+              <Icon as={CgPoll} boxSize={5} />
+              <Icon as={BsEmojiHeartEyes} boxSize={5} />
+              <Icon as={MdSchedule} boxSize={5} />
+              <Icon as={IoLocationOutline} boxSize={5} />
+            </HStack>
+            <Button className={style.tweetBtn}> Tweet </Button>
           </HStack>
         </GridItem>
       </Grid>
