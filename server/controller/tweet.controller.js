d6970b64b4d84c6a47e3c965581d4706cbfa6f29
AnandRP2030 <anand.rp2030@gmail.com>
2023-05-30 19:23:10 +0530

    edit like retweet and views update routes added in ther server

--- server/controller/tweet.controller.js
+++ server/controller/tweet.controller.js
@@ -1,6 +1,6 @@
 const TweetModel = require("../models/tweet.model.js");
 const RegistrationModel = require("../models/user.model.js");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 createTweet = async (req, res) => {
   try {
     let { content, imageUrls, audience = "Everyone" } = req.body;
@@ -55,42 +55,74 @@ const findOwner = async (tweets) => {
   return modifiedOwners;
 };
 
-//delete
+const findTweet = async (tweetId) => {
+  let validTweetId = mongoose.isValidObjectId(tweetId);
+  if (validTweetId) {
+    let tweet = await TweetModel.findById(tweetId);
+    if (!tweet) {
+      return res.status(404).send({ error: "Tweet not found" });
+    } else {
+      return tweet;
+    }
+  } else {
+    return res.status(404).send({ error: "Invalid tweet Id" });
+  }
+};
+
 deleteTweet = async (req, res) => {
   try {
-    
     const tweetId = req.params.tweetId;
-    let validTweetId = mongoose.isValidObjectId(tweetId);
-    console.log(validTweetId, 'valide')
-    let deletingTweet;
-    if (validTweetId) {
+    let deletingTweet = await findTweet(tweetId);
 
-      deletingTweet = await TweetModel.find({ _id: tweetId });
-      if (!deletingTweet || deletingTweet.length === 0) {
-        return res.status(404).send({ error: "Tweet not found" });
-      }
-    } else {
-      return res.status(404).send({ error: "Invalid tweet Id" });
-    }
-
-    console.log(deletingTweet, "idd");
     const requestedUserId = req.user.userId;
 
-    const tweetOwnerId = deletingTweet[0].owner.toString();
+    const tweetOwnerId = deletingTweet.owner.toString();
     if (tweetOwnerId === requestedUserId) {
       const deletedTweet = await TweetModel.deleteOne({ _id: tweetId });
-      console.log("delted tweet", deletedTweet);
-      res
-        .status(204)
-        .send({ message: "Tweet Successfully Deleted", tweet: deletedTweet });
-    } else {
-      res
-        .status(403)
-        .send({ error: "You don't have the persmission to delete the tweet" });
+      return res.status(204).json({ message: "Tweet Successfully Deleted" });
     }
+    res
+      .status(403)
+      .send({ error: "You don't have the persmission to delete the tweet" });
   } catch (err) {
     return res.status(500).send({ error: err });
   }
 };
 
-module.exports = { createTweet, getTweets, deleteTweet };
+editTweet = async (req, res) => {
+  try {
+    const tweetId = req.params.tweetId;
+    let editingTweet = await findTweet(tweetId);
+    const requestedUserId = req.user.userId;
+    const tweetOwnerId = editingTweet.owner.toString();
+
+    if (requestedUserId === tweetOwnerId) {
+      const { likes, retweets, views, content } = req.body;
+      let updatedValue;
+      if (likes) {
+        updatedValue = await TweetModel.findByIdAndUpdate(tweetId, { likes });
+      }
+
+      if (content) {
+        updatedValue = await TweetModel.findByIdAndUpdate(tweetId, { content });
+      }
+
+      if (retweets) {
+        updatedValue = await TweetModel.findByIdAndUpdate(tweetId, {
+          retweets,
+        });
+      }
+      if (views) {
+        updatedValue = await TweetModel.findByIdAndUpdate(tweetId, { views });
+      }
+
+      res.status(200).send(updatedValue);
+    } else {
+      res.status(403).send({ error: "you don't have the permission to edit" });
+    }
+  } catch (error) {
+    return res.status(500).send({ error: error });
+  }
+};
+
+module.exports = { createTweet, getTweets, deleteTweet, editTweet };
--- server/routes/tweet.router.js
+++ server/routes/tweet.router.js
@@ -1,4 +1,4 @@
-const { createTweet , getTweets, deleteTweet} = require('../controller/tweet.controller');
+const { createTweet , getTweets, deleteTweet, editTweet} = require('../controller/tweet.controller');
 const {findOwner}  = require('../middlewares/tweet.middleware')
 const tweetRouter = require('express').Router();
 
@@ -6,5 +6,5 @@ const tweetRouter = require('express').Router();
 tweetRouter.get('/', getTweets);
 tweetRouter.post('/create', createTweet);
 tweetRouter.delete('/delete/:tweetId', deleteTweet);
-
+tweetRouter.patch('/edit/:tweetId', editTweet);
 module.exports = {tweetRouter}
